590ed3595d874d4e50b6dc98292e80f5add4ab31
Eufranio <frani@magitechserver.com>
2017-11-27 21:48:36 -0200

    Forgot to commit the utils class...

--- src/main/java/net/kaikk/mc/gpp/Utils.java
+++ src/main/java/net/kaikk/mc/gpp/Utils.java
@@ -7,14 +7,9 @@ public class Utils {
 	private Utils() {}
 	
 	public static boolean isFakePlayer(Player player) {
-		/*if (player.getName().charAt(0) != '[') {
-			for (Player p : Bukkit.getOnlinePlayers()) {
-				if (player==p) {
-					return false;
-				}
-			}
+		if (player.hasPlayedBefore()) {
+			return false;
 		}
-		return true;*/
-		return false;
+		return true;
 	}
 }
